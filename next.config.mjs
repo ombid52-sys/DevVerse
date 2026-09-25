6bd7bf98e0898baca97c5bc4a644be9407ba290d
Om Bhardwaj <ombid52@gmail.com>
2026-09-25 23:11:47 +0530

    Fix Render Build Dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   experimental: {
     serverActions: {
       bodySizeLimit: "60mb", // Allow ZIP uploads up to 50MB
